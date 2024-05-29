9a12aad42df34ca7866ff4d8e61dc1b3a7ddb5cb
황선미 <a.@ui-MacBookPro.local>
2024-05-29 23:16:47 +0900

    0529

---
+++ dist/assets/view-ghy2G0v3.js
@@ -0,0 +1,1 @@
+import"./main-BQvHxN61.js";import{Fancybox as a}from"https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.esm.js";a.bind('[data-fancybox="gallery"]',{});
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,28 @@
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+//     build: {
+//         rollupOptions: {
+//             input: {
+//                 main: 'index.html',
+//                 work: 'work.html',
+//                 about: 'about.html',
+//                 view: 'view.html',
+//             }
+//         }
+//     }
+// });
 import { defineConfig } from 'vite';
+import path from 'path';
+
 export default defineConfig({
     build: {
         rollupOptions: {
             input: {
-                main: 'index.html',
-                work: 'work.html',
-                about: 'about.html',
-                view: 'view.html',
+                main: path.resolve(__dirname, 'index.html'),
+                work: path.resolve(__dirname, 'work.html'),
+                about: path.resolve(__dirname, 'about.html'),
+                view: path.resolve(__dirname, 'view.html'),
             }
         }
     }
